39582595f23b7b3127dca12f8be9885f996360b7
iambrasco <ebangabdon641@gmail.com>
2024-07-12 16:59:30 +0900

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import React from 'react';
 
 function Home() {
+    const  loggedIn = {firstName: 'Abdon', lastName: 'EBANG ELLA', email: 'contact@eeacoder.uk'};
   return (
-    <div>
-      Home
-    </div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+            <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user={loggedIn?.firstName || 'Guest'}
+            subtext='Access and manage your account and transactions efficiently.'
+            />
+            <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+            />
+        </header>
+        RECENET TRANSACTIONS
+      </div>
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 123.50},{currentBalance: 500.50}]}
+      />
+    </section>
   )
 }
 
